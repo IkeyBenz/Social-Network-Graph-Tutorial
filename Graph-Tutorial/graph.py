885de48366b2677130bf3a8ee77299411c51831c
Ikey Benzaken <ikey.benz@gmail.com>
2019-07-30 15:56:42 -0700

    Updated graph class

--- Graph-Tutorial/graph.py
+++ Graph-Tutorial/graph.py
@@ -1,118 +1,102 @@
 #!python
 
-""" Vertex Class
-A helper class for the Graph class that defines vertices and vertex neighbors.
-"""
-
-
-class Vertex(object):
-
-    def __init__(self, vertex):
-        """initialize a vertex and its neighbors
-
-        neighbors: set of vertices adjacent to self,
-        stored in a dictionary with key = vertex,
-        value = weight of edge between self and neighbor.
-        """
-        self.id = vertex
-        self.neighbors = {}
-
-    def addNeighbor(self, vertex, weight=0):
-        """add a neighbor along a weighted edge"""
-        # TODO check if vertex is already a neighbot
-        # TODO if not, add vertex to neighbors and assign weight.
-
-    def __str__(self):
-        """output the list of neighbors of this vertex"""
-        return str(self.id) + " adjancent to " +
-        str([x.id for x in self.neighbors])
-
-    def getNeighbors(self):
-        """return the neighbors of this vertex"""
-        # TODO return the neighbors
-
-    def getId(self):
-        """return the id of this vertex"""
-        return self.id
-
-    def getEdgeWeight(self, vertex):
-        """return the weight of this edge"""
-        # TODO return the weight of the edge from this
-        vertext to the given vertex.
-
-
 """ Graph Class
 A class demonstrating the essential
 facts and functionalities of graphs.
 """
 
+from vertex import Vertex
+
 
 class Graph:
-    def __init__(self):
-        """ initializes a graph object with an empty dictionary.
-        """
-        self.vertList = {}
-        self.numVertices = 0
+    def __init__(self, g_type):
+        """ Initializes a graph object with an empty dictionary """
+        self.vertices = {}
+        self.num_vertices = 0
+        self.directional = g_type == 'd'
+        self.edges = []
 
-    def addVertex(self, key):
-        """add a new vertex object to the graph with
+    def add_vertex(self, key):
+        """
+        Adds a new vertex object to the graph with
         the given key and return the vertex
         """
-        # TODO increment the number of vertices
-        # TODO create a new vertex
-        # TODO add the new vertex to the vertex list
-        # TODO return the new vertex
+        self.num_vertices += 1
+        new_vertex = Vertex(key)
+        self.vertices[key] = new_vertex
 
-    def getVertex(self, key):
-        """return the vertex if it exists"""
-        # TODO return the vertex if it is in the graph
+        return new_vertex
 
-    def addEdge(self, f, t, cost=0):
-        """add an edge from vertex f to vertex t with a cost
+    def get_vertex(self, key):
+        """ Returns the vertex if it exists """
+        return self.vertices[key]
+
+    def add_edge(self, start, end, cost=0):
+        """ Adds an edge from vertex f to vertex t with a cost """
+        if start not in self.vertices:
+            self.add_vertex(start)
+        if end not in self.vertices:
+            self.add_vertex(end)
+
+        self.vertices[start].add_neighbor(self.vertices[end], cost)
+        self.edges.append((start, end, cost))
+
+    def get_vertices(self):
+        """ Returns all the vertices in the graph """
+
+        return self.vertices.keys()
+
+    def breadth_first_traversal(self, start):
         """
-        # TODO if either vertex is not in the graph,
-        # add it - or return an error (choice is up to you).
-        # TODO if both vertices in the graph, add the
-        # edge by making t a neighbor of f
-        # and using the addNeighbor method of the Vertex class.
-        # Hint: the vertex f is stored in self.vertList[f].
+           Uses breadth first search to find the shortest path
+           between two vertices.
+        """
 
-    def getVertices(self):
-        """return all the vertices in the graph"""
-        return self.vertList.keys()
+        queue = [start]  # A 'queue' for storing vertices to check later
+        seen = {start}  # A set of all the seen vertices
+
+        while len(queue):
+            for vertice in self.vertices[queue.pop(0)].get_neighbors():
+                if vertice.id not in seen:
+                    yield vertice.id
+                    seen.add(vertice.id)
+                    queue.append(vertice.id)
+
+    def shortest_path_between(self, start, end):
+
+        connecting_vertices = []
+
+        for vertex in self.breadth_first_traversal(start):
+            connecting_vertices.append(vertex)
+
+            if vertex == end:
+                return connecting_vertices
+
+        return ValueError(f"{end} is not a vertex in this graph.")
+
+    def depth_first_traversal(self, start, end, visit, seen=None):
+        """
+            Uses depth first search to traverse the graph from a
+            start vertex.
+        """
+
+        if seen is None:
+            seen = {start}
+
+        vert = self.vertices[start]
+        visit(vert)
+
+        for neighbor in vert.get_neighbors():
+            if neighbor not in seen:
+                if neighbor is end:
+                    return visit(self.vertices[end])
+                else:
+                    seen.add(neighbor)
+                    return self.depth_first_traversal(neighbor, end, visit, seen)
 
     def __iter__(self):
-        """iterate over the vertex objects in the
-        graph, to use sytax: for v in g
         """
-        return iter(self.vertList.values())
-
-
-# Driver code
-
-
-if __name__ == "__main__":
-
-    # Challenge 1: Create the graph
-
-    g = Graph()
-
-    # Add your friends
-    g.addVertex("Friend 1")
-    g.addVertex("Friend 2")
-    g.addVertex("Friend 3")
-
-    # ...  add all 10 including you ...
-
-    # Add connections (non weighted edges for now)
-    g.addEdge("Friend 1", "Friend 2")
-    g.addEdge("Friend 2", "Friend 3")
-
-    # Challenge 1: Output the vertices & edges
-    # Print vertices
-    print("The vertices are: ", g.getVertices(), "\n")
-
-    print("The edges are: ")
-    for v in g:
-        for w in v.getNeighbors():
-            print("( %s , %s )" % (v.getId(), w.getId()))
+        Iterates over the vertex objects in the graph.
+        Allows sytax: for v in g
+        """
+        return iter(self.vertices.values())
